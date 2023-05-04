ac2ac9f25d48b7ea38bea3a495b03f1225326d3b
Skylar <skylarwdorn@gmail.com>
2023-05-03 20:21:18 -0700

    I'd say about 25% done but I need to take a break because I keep getting lost in my code.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # employee-pwd-generator
+
+## Description
+A Javascript function that generates a randomized password that's based on customized criteria
+
+I made a function that can take user prompts and develop a password accordingly. My biggest motivation for this was to practice my function creation abilities.
+
+My greatest roadblock for this project would be 
+
+## Deployed Page link
+https://skylardorn.github.io/employee-pwd-generator/
+
+## Installation
+N/A 
+
+
+## License
+Open Source
--- script.js
+++ script.js
@@ -1,6 +1,55 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// password criteria bank
+let upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const lowerCase = ['a','b','c','d','e','f','d','e','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const specialChar = ['!','@','#','$','%','^','&','*', ')', '(', '_', '+', '-', '<', '>'];
+const num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+
+
+function generatePassword() {
+  let upperCase = confirm("Would you like to add capital letters?");
+  let lowerCase = confirm("Would you like to add lowercase letters?");
+  let specialChar = confirm("Would you like to add special characters?");
+  let num = confirm("Would you like to add numbers?");
+
+  if (upperCase) {
+    
+  }
+
+};
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,91 +58,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-function generatePassword() {
-  let upperCase = prompt("Would you like to add capital letters?");
-  let lowerCase = prompt("Would you like to add lowercase letters?");
-  let specialChar = prompt("Would you like to add special characters?");
-  let num = prompt("Would you like to add numbers?");
-
-  if (upperCase) {
-    let upperCase = [
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-      "G",
-      "H",
-      "I",
-      "J",
-      "K",
-      "L",
-      "M",
-      "N",
-      "O",
-      "P",
-      "Q",
-      "R",
-      "S",
-      "T",
-      "U",
-      "V",
-      "W",
-      "X",
-      "Y",
-      "Z",
-    ];
-
-    const random = Math.floor(Math.random() * upperCase.length);
-    console.log(random, upperCase[random]);
-  }
-
-  if (lowerCase) {
-    let lowerCase = [
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f",
-      "g",
-      "h",
-      "i",
-      "j",
-      "k",
-      "l",
-      "m",
-      "n",
-      "o",
-      "p",
-      "q",
-      "r",
-      "s",
-      "t",
-      "u",
-      "v",
-      "w",
-      "x",
-      "y",
-      "z",
-    ];
-
-    const random = Math.floor(Math.random() * lowerCase.length);
-    console.log(random, lowerCase[random]);
-  }
-
-  if (specialChar) {
-    const random = Math.floor(Math.random() * specialChar.length);
-    console.log(random, specialChar[random]);
-  }
-
-  if (num) {
-    let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    const random = Math.floor(Math.random() * num.length);
-    console.log(random, num[random]);
-  }
-}
 
 generateBtn.addEventListener('click', writePassword());
